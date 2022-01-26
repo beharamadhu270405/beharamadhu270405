f4bb3e271b8fb02c2f1a082e7edbe1eaac23d943
Madhu Prakash <beharamadhu270405@gmail.com>
2022-01-27 00:05:16 +0530

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <TaskButton text="Add Task" color="green" />
+    <TaskButton text="Update Task" color="black" />
+    <TaskButton text="Delete Task" color="red" />
+  </header>
+</template>
+
+<script>
+import TaskButton from "./TaskButton.vue";
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "Task Tracker",
+    },
+  },
+  components: {
+    TaskButton,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/TaskButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <button class="btn" :style="{ background: color }" @click="onClick()">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TaskButton",
+  props: {
+    text: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+  },
+  methods: {
+    onClick() {
+      console.log("Hello");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
